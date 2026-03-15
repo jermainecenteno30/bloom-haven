200f67e5ecef4db936d0cb327f7d08b90e20242b
Jermaine Centeno <jermaine.centeno30@gmail.com>
2026-03-15 21:47:25 +0800

    Update db.php and remove test.php & port_test.php

--- config/db.php
+++ config/db.php
@@ -1,11 +1,10 @@
 <?php
-$host = getenv('DB_HOST');         // FreeDB server
-$user = getenv('DB_USER');         // FreeDB username
-$password = getenv('DB_PASSWORD'); // FreeDB password
-$database = getenv('DB_NAME');     // FreeDB database
-$port = getenv('DB_PORT');         // FreeDB port
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "bloom_haven";
 
-$conn = new mysqli($host, $user, $password, $database, $port);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
